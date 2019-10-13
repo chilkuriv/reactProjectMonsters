45f620c8d01290b41df56e8da6cecb53b6da5260
System Administrator <root@Imperio.local>
2019-10-13 02:00:14 -0400

    Updates

---
+++ precache-manifest.6ba81c78f34906531bd4e129b09e82e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "692bb33caa1ed8804543a469c9a6837c",
+    "url": "/reactProjectMonsters/index.html"
+  },
+  {
+    "revision": "a3b3b6f9a0e4be13f687",
+    "url": "/reactProjectMonsters/static/css/main.7521385b.chunk.css"
+  },
+  {
+    "revision": "102a9b24fd9babe5b595",
+    "url": "/reactProjectMonsters/static/js/2.a393edc3.chunk.js"
+  },
+  {
+    "revision": "a3b3b6f9a0e4be13f687",
+    "url": "/reactProjectMonsters/static/js/main.40226371.chunk.js"
+  },
+  {
+    "revision": "468e4a0a4ed67356caae",
+    "url": "/reactProjectMonsters/static/js/runtime-main.e8ee8a4e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/reactProjectMonsters/static/media/logo.25bf045c.svg"
+  }
+]);
